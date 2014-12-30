9b7c7852d8539115240ab21936fa9761d8bb321b
Joel Scoble <joel.scoble@outlook.com>
2014-12-30 13:16:37 -0600

    add copyright info and attribution

--- flag.go
+++ flag.go
@@ -1,3 +1,11 @@
+// Copyright 2014, Joel Scoble, github.com/mohae
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+// This file is based on Go's log package and the original
+// copyright notice follows. No additional claims or warranties,
+// implied or otherwise are made by Joel Scoble.
+//
 // Copyright 2009 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
